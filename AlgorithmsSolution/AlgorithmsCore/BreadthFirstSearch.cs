1e905fb0b25983179d82a9a8c83153af095e0965
Igor Tomic <igortomic81@gmail.com>
2021-06-02 18:28:40 +0200

    Breadth First Search

---
+++ AlgorithmsSolution/AlgorithmsCore/BreadthFirstSearch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsCore
+{
+    public static class BreadthFirstSearch
+    {
+        public static string DoBFS(Dictionary<string, List<string>> graph) {
+            Queue<string> searchQueue = new Queue<string>();
+            List<string> alreadySearched = new List<string>();
+            foreach (var neighbour in graph["startnode"])
+            {
+                searchQueue.Enqueue(neighbour);
+            }
+            while (searchQueue.Count>0)
+            {
+                var node = searchQueue.Dequeue();
+                if (node.Contains("Mango seller"))
+                {
+                    return node;
+                }
+                else
+                {
+                    if (!alreadySearched.Contains(node))
+                    {
+                        alreadySearched.Add(node);
+                        if (graph[node]?.Count > 0)
+                        {
+                            foreach (var neighbour in graph[node])
+                            {
+                                searchQueue.Enqueue(neighbour);
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
--- AlgorithmsSolution/AlgorithmsUnitTest/AlgorithmsUnitTest.cs
+++ AlgorithmsSolution/AlgorithmsUnitTest/AlgorithmsUnitTest.cs
@@ -26,7 +26,7 @@ namespace AlgorithmsUnitTest
         {
             List<int> unsortedList = new List<int>() { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
             List<int> expectedSortedList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            CollectionAssert.AreEqual(expectedSortedList, AlgorithmsCore.Selection_Sort.DoMinSelectionSort(unsortedList));
+            CollectionAssert.AreEqual(expectedSortedList, AlgorithmsCore.SelectionSort.DoMinSelectionSort(unsortedList));
 
         }
 
@@ -36,6 +36,22 @@ namespace AlgorithmsUnitTest
             List<int> unsortedList = new List<int>() { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
             List<int> expectedSortedList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             CollectionAssert.AreEqual(expectedSortedList, AlgorithmsCore.QuickSort.DoQuickSort(unsortedList));
+
+        }
+
+        [TestMethod]
+        public void TestBFS()
+        {
+            Dictionary<string, List<string>> graph = new Dictionary<string, List<string>>();
+            graph.Add("startnode", new List<string>() { "alice", "bob", "claire" });
+            graph.Add("alice", new List<string>() { "peggy" });
+            graph.Add("bob", new List<string>() { "anuj", "peggy" });
+            graph.Add("claire", new List<string>() { "thom Mango seller", "jonny" });
+            graph.Add("anuj", null);
+            graph.Add("peggy", null);
+            graph.Add("thom  Mango seller", null);
+            graph.Add("jonny", null);
+            Assert.AreEqual("thom Mango seller", AlgorithmsCore.BreadthFirstSearch.DoBFS(graph));
 
         }
     }
